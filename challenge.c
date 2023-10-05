e8ed3510ba5009c39c511f43351ac544b1ab9c19
NonoLecal <147018170+NonoLecal@users.noreply.github.com>
2023-10-05 15:37:39 +1100

    Create challenge.c
    Mon tout premier CTF, bon courage à tous !

---
+++ challenge.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Définir le mot de passe correct
+    char motDePasseCorrect[] = "NonoLePlusBeau";
+            
+    // Demander à l'utilisateur de saisir le mot de passe
+    char motDePasseSaisi[100]; // Vous pouvez ajuster la taille selon vos besoins
+    printf("Veuillez entrer le mot de passe : ");
+    scanf("%s", motDePasseSaisi);
+    
+    // Comparer le mot de passe saisi avec le mot de passe correct
+    if (strcmp(motDePasseSaisi, motDePasseCorrect) == 0) {
+        printf("DINUM{FakeFlag}\n"); // Mot de passe correct
+    } else {
+        printf("Pkus de chance la prochaine fois\n"); // Mot de passe incorrect
+    }
+    return 0;
+}
